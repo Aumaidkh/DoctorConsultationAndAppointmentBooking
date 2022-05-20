37273942b958e8f3fddf9bcf2ec05d197978d755
Aumaidkh <52782821+Aumaidkh@users.noreply.github.com>
2022-05-20 10:25:59 +0530

    Added Date Recyclerview with radio behavior on item clicks

--- app/src/main/java/com/android/doctorce/feature_book_appointment/presentation/ui/doctor_details/adapters/AppointmentDateAdapter.kt
+++ app/src/main/java/com/android/doctorce/feature_book_appointment/presentation/ui/doctor_details/adapters/AppointmentDateAdapter.kt
@@ -10,56 +10,69 @@ import com.android.doctorce.R
 import com.android.doctorce.databinding.DoctorChipLayoutBinding
 import com.android.doctorce.feature_book_appointment.presentation.ui.doctor_details.AppointmentDateUiState
 
-class AppointmentDateAdapter: ListAdapter<AppointmentDateUiState,AppointmentDateAdapter.AppointmentDateViewHolder>(
-    DIFF_UTIL_CALLBACK){
+class AppointmentDateAdapter :
+    RecyclerView.Adapter<AppointmentDateAdapter.AppointmentDateViewHolder>() {
+
+    var items = emptyList<AppointmentDateUiState>()
+    var selectedItem: AppointmentDateUiState? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppointmentDateViewHolder {
-        return from(parent)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = DataBindingUtil.inflate<DoctorChipLayoutBinding>(
+            inflater,
+            R.layout.doctor_chip_layout,
+            parent,
+            false
+        )
+        return AppointmentDateViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: AppointmentDateViewHolder, position: Int) {
-        val currentItem = getItem(position)
+        val currentItem = items[position]
         holder.bind(currentItem)
     }
 
-    class AppointmentDateViewHolder(private val binding: DoctorChipLayoutBinding): RecyclerView.ViewHolder(binding.root){
+    private fun deselectTheRestElements() {
+        items.forEach { item ->
+            item.isSelected = false
+        }
+    }
 
-        fun bind(item: AppointmentDateUiState){
+    inner class AppointmentDateViewHolder(private val binding: DoctorChipLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: AppointmentDateUiState) {
             binding.tvDay.text = item.day
             binding.tvDate.text = item.date
-            if (item.isSelected){
+            if (item.isSelected) {
+                //   deselectTheRestElements()
                 binding.apply {
                     root.setBackgroundResource(R.drawable.primary_chip_background)
                     tvDate.setTextColor(itemView.context.getColor(R.color.white))
                     tvDay.setTextColor(itemView.context.getColor(R.color.white))
                 }
+            } else {
+                binding.apply {
+                    item.isSelected = false
+                    root.setBackgroundResource(R.drawable.white_chip_background)
+                    tvDate.setTextColor(itemView.context.getColor(R.color.primary_color))
+                    tvDay.setTextColor(itemView.context.getColor(R.color.primary_color))
+                }
             }
 
             itemView.setOnClickListener {
+                deselectTheRestElements()
+                selectedItem = item
                 item.isSelected = !item.isSelected
-                bind(item)
+                notifyDataSetChanged()
             }
         }
 
     }
 
-    companion object {
+    override fun getItemCount(): Int = items.size
 
-        val DIFF_UTIL_CALLBACK = object : DiffUtil.ItemCallback<AppointmentDateUiState>(){
-            override fun areItemsTheSame(
-                oldItem: AppointmentDateUiState,
-                newItem: AppointmentDateUiState
-            ) = oldItem === newItem
-
-            override fun areContentsTheSame(
-                oldItem: AppointmentDateUiState,
-                newItem: AppointmentDateUiState
-            ) = oldItem.day == newItem.day
-        }
-        fun from(parent: ViewGroup): AppointmentDateViewHolder {
-            val inflater = LayoutInflater.from(parent.context)
-            val binding = DataBindingUtil.inflate<DoctorChipLayoutBinding>(inflater, R.layout.doctor_chip_layout,parent,false)
-            return AppointmentDateViewHolder(binding)
-        }
+    fun submitList(items: List<AppointmentDateUiState>) {
+        this.items = items
     }
 }
